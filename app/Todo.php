454d30864d521b9945dee02bb32f19c942148d18
海野遼 <>
2019-11-18 12:04:26 +0900

    問題２を初期状態に修正

--- app/Todo.php
+++ app/Todo.php
@@ -3,16 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Todo extends Model
 {
-    use SoftDeletes;
 
     protected $fillable = [
         'title',
         'user_id',
-        'deleted_at'
     ];
     // ここから
     public function getByUserId($id)
